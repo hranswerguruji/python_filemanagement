523118975ce0805d1fdb0d2fcc7c7343dfcc6b67
hranswerguruji <hranswerguruji@gmail.com>
2024-05-26 09:25:51 +0530

    Write on file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-with open("my_file.txt") as file:
-    content = file.read()
+# with open("my_file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+with open("my_file.txt", mode = "w") as file:
+    content = file.write("New text for me")
     print(content)
